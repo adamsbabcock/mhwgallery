e26ed4d0c2ddba612b5297982dac9faf7277ce61
ababcock <adamsbabcock@gmail.com>
2020-05-21 10:55:37 -0700

    added spacing for main page header

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
 
 export default class PageHeader extends Component {
   render() {
     const { pageTitle } = this.props
 
     return (
-      <div>
+      <Box pt={5}>
         <Typography variant="h2" component="h1" align="center" gutterBottom>
           {pageTitle}
         </Typography>
-      </div>
+      </Box>
     )
   }
 }
